4a78964ad25e37553b57ba1f40d27cf84e21b6c4
Sanjito Kurniawan <ssi@hellofresh.com>
2020-10-02 13:55:16 +0200

    Reduce jmeter resource requests in integration-test

--- ci/integration-tests.sh
+++ ci/integration-tests.sh
@@ -4,6 +4,16 @@ set -e
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
 
+# Adjust resource requests and limits
+#export JMETER_MASTER_CPU_LIMITS="2000m"
+export JMETER_MASTER_CPU_REQUESTS="150m"
+#export JMETER_MASTER_MEMORY_LIMITS="4Gi"
+export JMETER_MASTER_MEMORY_REQUESTS="500Mi"
+#export JMETER_WORKER_CPU_LIMITS="2000m"
+export JMETER_WORKER_CPU_REQUESTS="150m"
+#export JMETER_WORKER_MEMORY_LIMITS="4Gi"
+export JMETER_WORKER_MEMORY_REQUESTS="500Mi"
+
 echo "Starting kangal proxy"
 ./bin/kangal proxy --kubeconfig="$HOME/.kube/config" --max-load-tests 1 > /tmp/kangal_proxy.log 2>&1 &
 PID_PROXY=$!
