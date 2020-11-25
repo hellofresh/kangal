d7401c93482d98951dcd73c3e3e2871748a72e45
Vladimir Garvardt <vgarvardt@gmail.com>
2020-11-25 14:55:26 +0100

    Use local version of protoc and plugins

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,8 @@
 // +build tools
 
-package tools
+// Place any runtime dependencies as imports in this file.
+// Go modules will be forced to download and install them.
+package main
 
 import (
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
