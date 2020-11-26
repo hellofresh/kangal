f5e083c50beb85c1ad0d3230849300dd21507db5
Vladimir Garvardt <vgarvardt@gmail.com>
2020-11-26 09:54:38 +0100

    Reverted tools.go as we need it to handle command versioning

---
+++ tools/tools.go
@@ -0,0 +1,12 @@
+// +build tools
+
+package main
+
+// Place any runtime dependencies as imports in this file.
+// Go modules will be forced to download and install them.
+import (
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
+	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+)
