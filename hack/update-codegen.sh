8074da0296f2018c558b7f621c314ea972b35c18
Sanjito Kurniawan <ssi@hellofresh.com>
2021-01-19 16:39:33 +0100

    Update codegen script to output in project directory

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -20,16 +20,22 @@ set -o pipefail
 
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+TEMP="$SCRIPT_ROOT/_tmp"
 
 # generate the code with:
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
-  github.com/hellofresh/kangal/pkg/kubernetes/generated github.com/hellofresh/kangal/pkg/kubernetes/apis \
+  "github.com/hellofresh/kangal/pkg/kubernetes/generated" \
+  "github.com/hellofresh/kangal/pkg/kubernetes/apis" \
   loadtest:v1 \
-   --output-base "./../../.." \
-  --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt \
+  --output-base "$TEMP" \
+  --go-header-file "$SCRIPT_ROOT/hack/boilerplate.go.txt" \
 
-# To use your own boilerplate text append:
-#   --go-header-file "${SCRIPT_ROOT}"/hack/custom-boilerplate.go.txt
+# Move generated code to project
+cp -R "$TEMP"/github.com/hellofresh/kangal/pkg/kubernetes/generated/* ./pkg/kubernetes/generated/
+cp -R "$TEMP"/github.com/hellofresh/kangal/pkg/kubernetes/apis/* ./pkg/kubernetes/apis/
+
+# Cleanup
+rm -rf "$TEMP"
