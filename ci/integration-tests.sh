8bff1fa11545d020429e8d56e724a4dae0cb7643
Svetlana Radyuk <svetlana.radyuk@gmail.com>
2021-06-08 16:19:01 +0200

    starting a local server and sending requests to it from a real test (#141)
    * starting a local server and sending requests to it from a real test
    
    * increasing watch timeout
    
    * remove extra timeouts
    
    * use different waitForResourceTimeout in tests and JMeter resoursces
    
    * update comments in integration-tests.sh

--- ci/integration-tests.sh
+++ ci/integration-tests.sh
@@ -4,6 +4,20 @@ set -e
 export AWS_ACCESS_KEY_ID=""
 export AWS_SECRET_ACCESS_KEY=""
 
+# Create a simple service to be called from the created loadTest
+# This service will log all the requests
+kubectl create ns test-busybox
+kubectl run --generator=run-pod/v1 busybox --namespace=test-busybox --port=8280 --image=busybox -- sh -c "echo 'Hello' > /var/www/index.html && httpd -f -p 8280 -h /var/www/ -vv"
+kubectl expose pod busybox --type=NodePort --namespace=test-busybox
+kubectl wait --for=condition=ready pod busybox -n test-busybox
+
+NODE_IP=$(kubectl describe pod busybox -n test-busybox | grep "Node:" | cut -d '/' -f 2)
+NODE_PORT=$(kubectl get service busybox -n test-busybox | grep busybox | cut -d ':' -f 2 | cut -d '/' -f 1)
+
+# Update JMX test to use node_ip:port to send requests
+sed -i "s/TEST_IP/$NODE_IP/g" ./pkg/controller/testdata/valid/integration_test.jmx
+sed -i "s/TEST_PORT/${NODE_PORT}/g" ./pkg/controller/testdata/valid/integration_test.jmx
+
 echo "Starting kangal proxy"
 ./bin/kangal proxy --kubeconfig="$HOME/.kube/config" --max-load-tests 1 >/tmp/kangal_proxy.log 2>&1 &
 PID_PROXY=$!
@@ -32,3 +46,13 @@ echo "Starting integration tests"
 
 # Run the integration tests
 KUBECONFIG="$HOME/.kube/config" make test-integration
+
+# Check the logs of busybox server and count the number of requests sent by JMeter.
+# integration_test.jmx is designed to send 30 requests during 30 seconds.
+# jmeter_integration_test.go creates a loadTest with 2 distributed pods, which leads us to 60 desired requests to the server
+DESIRED_REQUESTS_COUNT=60
+REQUEST_COUNT=$(kubectl logs busybox -n test-busybox | grep -c "response:200")
+if [ "${REQUEST_COUNT}" -ne "${DESIRED_REQUESTS_COUNT}" ]; then
+  echo "JMeter Integration Test sent $REQUEST_COUNT requests, but $DESIRED_REQUESTS_COUNT requests were expected. Test failed."
+  exit 1
+fi
