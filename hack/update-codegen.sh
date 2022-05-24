db9812198b18f5bb75641e9b4c047f9561c0f37a
Amanda Hager Lopes de Andrade Katz <amanda.katz@serpro.gov.br>
2022-05-24 11:32:01 -0300

    Add verify-codegen to Makefile and update doc (#214)

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,6 +18,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+
 SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 TEMP="$SCRIPT_ROOT/_tmp"
@@ -26,7 +27,7 @@ TEMP="$SCRIPT_ROOT/_tmp"
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-"${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
+/bin/bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
   "github.com/hellofresh/kangal/pkg/kubernetes/generated" \
   "github.com/hellofresh/kangal/pkg/kubernetes/apis" \
   loadtest:v1 \
@@ -36,6 +37,3 @@ TEMP="$SCRIPT_ROOT/_tmp"
 # Move generated code to project
 cp -R "$TEMP"/github.com/hellofresh/kangal/pkg/kubernetes/generated/* ./pkg/kubernetes/generated/
 cp -R "$TEMP"/github.com/hellofresh/kangal/pkg/kubernetes/apis/* ./pkg/kubernetes/apis/
-
-# Cleanup
-rm -rf "$TEMP"
