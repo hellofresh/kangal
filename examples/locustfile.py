603bfaa9a215bb5d146cfdc63042bfa5e31499e1
eliflores <elizabeth.flores@hellofresh.com>
2022-12-23 11:58:45 +0100

    Update the locust example
    To change the conditions used in the example, format imports, and remove an unused variable.

--- examples/locustfile.py
+++ examples/locustfile.py
@@ -1,14 +1,18 @@
-import glob, tarfile, requests, os
+import glob
+import os
+import requests
+import tarfile
 
 from locust import HttpUser, task, between, events, runners
 from locust.runners import MasterRunner
 
+
 @events.quitting.add_listener
 def hook_quit(environment):
     presigned_url = os.environ.get('REPORT_PRESIGNED_URL')
-    if None == presigned_url:
+    if presigned_url is None:
         return
-    if False == isinstance(environment.runner, MasterRunner):
+    if not isinstance(environment.runner, MasterRunner):
         return
     report = '/home/locust/report.tar.gz'
     tar = tarfile.open(report, 'w:gz')
@@ -17,7 +21,8 @@ def hook_quit(environment):
         tar.add(item, arcname=os.path.basename(item))
     tar.close()
     request_headers = {'content-type': 'application/gzip'}
-    r = requests.put(presigned_url, data=open(report, 'rb'), headers=request_headers)
+    requests.put(presigned_url, data=open(report, 'rb'), headers=request_headers)
+
 
 class ExampleLoadTest(HttpUser):
     wait_time = between(5, 15)
