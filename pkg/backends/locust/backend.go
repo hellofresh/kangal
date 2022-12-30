8fb1a72491c45cf71cf45aa7337e17c5c84891b6
Thiago Arrais <thiago.arrais@gmail.com>
2022-12-30 10:36:57 -0300

    fix: makes the linter happy

--- pkg/backends/locust/backend.go
+++ pkg/backends/locust/backend.go
@@ -51,6 +51,7 @@ func (*Backend) Type() loadTestV1.LoadTestType {
 	return loadTestV1.LoadTestTypeLocust
 }
 
+// UsesCSVTestData must signal whether the testdata files can be assumed to be CSV
 func (*Backend) UsesCSVTestData() bool {
 	return false
 }
